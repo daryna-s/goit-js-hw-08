572e77928dc258196d7905e8f656dab37fe1c0a4
daryna-s <lypenn@gmail.com>
2022-09-04 13:24:46 +0300

    done task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+var throttle = require('lodash.throttle');
+
+// Инициализируй плеер в файле скрипта как это 
+// описано в секции pre - existing player,
+// но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
@@ -12,16 +18,25 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-
+// Разбери документацию метода on() и начни отслеживать 
+// событие timeupdate - обновление времени воспроизведения.
+// Сохраняй время воспроизведения в локальное хранилище. 
+// Пусть ключом для хранилища будет строка "videoplayer-current-time".
 
 const STORAGE_KEY = 'videoplayer-current-time';
 
-player.on('eventName', function (data) {
-  // data is an object containing properties specific to that event
-});
+player.on('timeupdate', throttle(function (time) {
 
+  localStorage.setItem(STORAGE_KEY, time.seconds);
+  
+}, 1000));
+
+
+// При перезагрузке страницы воспользуйся методом setCurrentTime()
+//  для того чтобы возобновить воспроизведение с сохраненной позиции.
+     
 player
-  .setCurrentTime(30.456)
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
   .then(function (seconds) {
     // seconds = the actual time that the player seeked to
   })
