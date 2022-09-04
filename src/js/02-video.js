11033922d43c5ea534408a343650293ebc9b359c
daryna-s <lypenn@gmail.com>
2022-09-04 12:07:58 +0300

    done task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('eventName', function (data) {
+  // data is an object containing properties specific to that event
+});
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,6 +4,7 @@ var throttle = require('lodash.throttle');
 
 const STORAGE_KEY = 'feedback-form-state';
 
+
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('submit', onFormSubmit);
@@ -13,7 +14,7 @@ populateFormData();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-
+  
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
@@ -24,16 +25,20 @@ function onFormDataInput(evt) {
   formData[evt.target.name] = evt.target.value;
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+
+  console.log(parsedFormData); 
 }
 
 function populateFormData() {
   const savedFormData = localStorage.getItem(STORAGE_KEY);
   const parsedFormData = JSON.parse(savedFormData);
-  console.log(parsedFormData);
 
   if (parsedFormData) {
     Object.entries(parsedFormData).forEach(([name, value]) => {
       form.elements[name].value = value;
     });
-  }
+  }  
 }
